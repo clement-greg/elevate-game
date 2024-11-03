6156c91b3b5a03caa9f786baeb747a39fe24f616
Greg Clement <gregb_clement@hotmail.com>
2024-11-02 20:24:44 -0600

    modified the lost screen

--- src/app/components/game-lost/game-lost.component.ts
+++ src/app/components/game-lost/game-lost.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { LottiePlayerComponent } from '../lottie-player/lottie-player.component';
 import { PressAComponent } from '../press-a/press-a.component';
 import { CommonModule } from '@angular/common';
@@ -13,9 +13,11 @@ import { CommonModule } from '@angular/common';
 export class GameLostComponent {
   showNextText = false;
   showPressA = false;
+  @ViewChild('jimmy') jimmy: LottiePlayerComponent;
   constructor() {
     setTimeout(() => this.showNextText = true, 1500);
     setTimeout(() => this.showPressA = true, 3500);
+    setTimeout(() => this.jimmy.play(), 2000);
   }
 
 }
